24d58985ec3e59b672b8beaebc5df69659aebf05
unknown <sumairasami2809@gmail.com>
2024-08-13 19:13:46 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const imageContainerEl = document.querySelector(".image-container");
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+let x = 0 ;
+let timer ;
+prevEl.addEventListener("click",()=>{
+    x = x + 45 ;
+    updateGallery()
+   
+});
+nextEl.addEventListener("click",()=>{
+    x = x - 45 ;
+    clearTimeout(timer);
+    updateGallery();
+   
+});
+function updateGallery(){
+    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(()=>{
+        x = x-45;
+        clearTimeout(timer);
+
+        updateGallery()
+
+    },3000)
+}
+updateGallery()
+
+
+// const imageContainerEl = document.querySelector(".image-container");
+// const prevEl = document.getElementById("prev");
+// const nextEl = document.getElementById("next");
+// let x = 0;
+// let timer;
+
+// prevEl.addEventListener("click", () => {
+//   x = x + 45;
+//   updateGallery();
+// });
+
+// nextEl.addEventListener("click", () => {
+//   x = x - 45;
+//   clearTimeout(timer);
+//   updateGallery();
+// });
+
+// function updateGallery() {
+//   const spans = imageContainerEl.querySelectorAll("span");
+//   spans.forEach((span, index) => {
+//     span.style.transform = `rotateY(${x + index * 45}deg) translateZ(400px)`;
+//   });
+//   timer = setTimeout(() => {
+//     x = x - 45;
+//     clearTimeout(timer);
+//     updateGallery();
+//   }, 3000);
+// }
+
+// updateGallery();
